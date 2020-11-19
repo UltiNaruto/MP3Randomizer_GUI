ba6f559a38f416a9206a24cfc7fe7ca04bb13f60
UltiNaruto <zenaruto@ymail.com>
2020-11-19 19:43:29 +0100

    Version 1.0.3 Fixed the unique save feature Updated the Readme

--- MP3Randomizer_GUI/Patcher/Patches/NTSC_U.cs
+++ MP3Randomizer_GUI/Patcher/Patches/NTSC_U.cs
@@ -6,7 +6,7 @@ namespace MP3Randomizer_GUI.Patcher.Patches
     {
         public override void SetSaveFilename(String filename)
         {
-            new DolPatch<String>(Main_Dol_Path, 0x8057EF90, filename).Apply();
+            new DolPatch<String>(Main_Dol_Path, 0x8057D898, filename).Apply();
         }
     }
 }
